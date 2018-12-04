3bac4c1ab575adafcff6c7d20c496c27f7b8f6f0
raakeshbhapkar <39795364+raakeshbhapkar@users.noreply.github.com>
2018-12-04 18:13:43 +0530

    Update emplist.component.ts

--- src/app/component/emplist/emplist.component.ts
+++ src/app/component/emplist/emplist.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
 import {Employee} from '../../models/employee.models'
 import {EmployeeService} from '../../share/emp.ser'
 import { Router,ActivatedRoute } from '@angular/router';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
 @Component({
   selector: 'app-emplist',
   templateUrl: './emplist.component.html',
   styleUrls: ['./emplist.component.css']
 })
+
 export class EmplistComponent implements OnInit {
 
 employees: Employee[] = [];
@@ -24,7 +26,7 @@ private _searchTerm: string;
     return this.employees.filter(employee =>
       employee.name.toLowerCase().indexOf(searchString.toLowerCase()) !== -1);
   }
-  constructor(private _EmployeeService:EmployeeService,private _router:Router) { 
+  constructor(private _EmployeeService:EmployeeService,private _router:Router,public dialog: MatDialog) { 
     
   }
 
@@ -41,14 +43,49 @@ private _searchTerm: string;
 
   }
     deleteEmployee(employeeId: number) {
+       console.log('delete');
     this._EmployeeService.deleteEmployee(employeeId);
     const i = this.filteredEmployees.findIndex(e => e.id === employeeId);
       if (i !== -1) {
         this.filteredEmployees.splice(i, 1);
       }
   }
+     openDialog=(employeeId: number)=> {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px'
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result=="ok"){
+        this.deleteEmployee(employeeId)
+      }
+    });
+  }
+ 
+  
 
 
+
+}
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  templateUrl: 'dialog.html',
+   styles: ['.btn-primary{margin-right:10px;}']
+})
+export class DialogOverviewExampleDialog{
+  check:boolean=false
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data) {}
+
+  onNoClick(): void {
+    this.dialogRef.close("close");
+  }
+  removeUser(): void {
+    this.check=true;
+    this.dialogRef.close("ok");
+
+  }
 
 }
 
