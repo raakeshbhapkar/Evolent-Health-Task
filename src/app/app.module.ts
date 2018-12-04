a3dc724ee1bdaaec5b9ef4e8d536b8f72809eb48
raakeshbhapkar <39795364+raakeshbhapkar@users.noreply.github.com>
2018-12-04 18:11:35 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {MatDialogModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
-import { EmplistComponent } from './component/emplist/emplist.component';
+import { EmplistComponent, DialogOverviewExampleDialog } from './component/emplist/emplist.component';
+
 import { CreateempComponent } from './component/createemp/createemp.component';
 import { EmployeeService } from './share/emp.ser';
 import { DisplayEmpComponent } from './component/display-emp/display-emp.component'
@@ -16,7 +18,7 @@ const appRoutes: Routes = [
   { path: 'list', component: EmplistComponent },
   { path: '', redirectTo: '/list', pathMatch: "full" },
   {path: 'edit/:id', component: CreateempComponent},
-  {path: 'pagenotfound', component: PageNotFoundComponent },
+  {path: '**', component: PageNotFoundComponent },
 ]
 @NgModule({
   declarations: [
@@ -24,19 +26,23 @@ const appRoutes: Routes = [
     EmplistComponent,
     CreateempComponent,
     DisplayEmpComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    DialogOverviewExampleDialog
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpModule,
     RouterModule,
     FormsModule,
+    MatDialogModule,
 
     RouterModule.forRoot(
       appRoutes
     )
 
   ],
+    entryComponents: [ DialogOverviewExampleDialog],
   providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
